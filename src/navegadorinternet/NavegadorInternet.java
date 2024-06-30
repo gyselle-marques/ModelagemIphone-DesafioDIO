ec4a8b0e9de9231f9dc8abe3e5c8fe7fd6aff0ea
Gyselle Marques <gmsilva619@gmail.com>
2024-06-30 19:45:44 -0300

    Update NavegadorInternet.java

--- src/navegadorinternet/NavegadorInternet.java
+++ src/navegadorinternet/NavegadorInternet.java
@@ -1,19 +1,5 @@
 package navegadorinternet;
 
-public class NavegadorInternet implements NavegadorInternetIntfc {
-
-    @Override
-    public void exibirPagina() {
-      System.out.println("Exibindo Pagina.");
-    }
-  
-    @Override
-    public void adicionarNovaAba(String pagina) {
-      System.out.println(pagina + " adicionada.");
-    }
-  
-    @Override
-    public void atualizarPagina() {
-      System.out.println("Atualizando Pagina.");
-    }
+public class NavegadorInternet extends NavegadorInternetAbst {
+    
 }
